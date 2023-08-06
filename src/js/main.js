bda9982040d9dac725a71159e044fb369a2dae0f
Marcel <marcelnedza2000@gmail.com>
2023-08-06 17:09:00 +0200

    nav desktop and nav mobile added and styled (styles and media for mobiles) added function to display menu with javascript when pressing burger button with hiding when anchor clicked

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-document.body.style.background = 'gold';
+const burgerBtn = document.querySelector('.nav-mobile__burger-btn');
+const mobileMenu = document.querySelector('.nav-mobile-menu');
+const navMobileItems = document.querySelectorAll('.nav-mobile__item');
+
+const toggleNav = () => {
+    mobileMenu.classList.toggle('nav-mobile-menu--active');
+}
+
+navMobileItems.forEach(item => {
+    item.addEventListener('click', () => {
+        mobileMenu.classList.remove('nav-mobile-menu--active');
+    })
+})
+
+burgerBtn.addEventListener('click', toggleNav);
